ec0a5c12c5e31071713c3bd1b64b2b6391ce9b98
Alexander Ionov <orlicus@gmail.com>
2015-07-16 09:24:02 +0400

    Lessons styling

--- Timetable/Timetable.Shared/Utils/ColorsHelper.cs
+++ Timetable/Timetable.Shared/Utils/ColorsHelper.cs
@@ -13,26 +13,26 @@ namespace Timetable.Utils
     {
         private static readonly List<Brush> ACCENT_COLORS = new List<Brush>()
         {
-            new SolidColorBrush(Color.FromArgb(100, 164, 196, 0)),
-            new SolidColorBrush(Color.FromArgb(100, 96, 169, 23)),
-            new SolidColorBrush(Color.FromArgb(100, 0, 138, 0)),
-            new SolidColorBrush(Color.FromArgb(100, 0, 171, 169)),
-            new SolidColorBrush(Color.FromArgb(100, 27, 161, 226)),
-            new SolidColorBrush(Color.FromArgb(100, 0, 80, 239)),
-            new SolidColorBrush(Color.FromArgb(100, 106, 0, 255)),
-            new SolidColorBrush(Color.FromArgb(100, 170, 0, 255)),
-            new SolidColorBrush(Color.FromArgb(100, 244, 114, 208)),
-            new SolidColorBrush(Color.FromArgb(100, 216, 0, 115)),
-            new SolidColorBrush(Color.FromArgb(100, 162, 0, 37)),
-            new SolidColorBrush(Color.FromArgb(100, 229, 20, 0)),
-            new SolidColorBrush(Color.FromArgb(100, 250, 104, 0)),
-            new SolidColorBrush(Color.FromArgb(100, 240, 163, 10)),
-            new SolidColorBrush(Color.FromArgb(100, 227, 200, 0)),
-            new SolidColorBrush(Color.FromArgb(100, 130, 90, 44)),
-            new SolidColorBrush(Color.FromArgb(100, 109, 135, 100)),
-            new SolidColorBrush(Color.FromArgb(100, 100, 118, 135)),
-            new SolidColorBrush(Color.FromArgb(100, 118, 96, 138)),
-            new SolidColorBrush(Color.FromArgb(100, 135, 121, 78))
+            new SolidColorBrush(Color.FromArgb(255, 164, 196, 0)),
+            new SolidColorBrush(Color.FromArgb(255, 96, 169, 23)),
+            new SolidColorBrush(Color.FromArgb(255, 0, 138, 0)),
+            new SolidColorBrush(Color.FromArgb(255, 0, 171, 169)),
+            new SolidColorBrush(Color.FromArgb(255, 27, 161, 226)),
+            new SolidColorBrush(Color.FromArgb(255, 0, 80, 239)),
+            new SolidColorBrush(Color.FromArgb(255, 106, 0, 255)),
+            new SolidColorBrush(Color.FromArgb(255, 170, 0, 255)),
+            new SolidColorBrush(Color.FromArgb(255, 244, 114, 208)),
+            new SolidColorBrush(Color.FromArgb(255, 216, 0, 115)),
+            new SolidColorBrush(Color.FromArgb(255, 162, 0, 37)),
+            new SolidColorBrush(Color.FromArgb(255, 229, 20, 0)),
+            new SolidColorBrush(Color.FromArgb(255, 250, 104, 0)),
+            new SolidColorBrush(Color.FromArgb(255, 240, 163, 10)),
+            new SolidColorBrush(Color.FromArgb(255, 227, 200, 0)),
+            new SolidColorBrush(Color.FromArgb(255, 130, 90, 44)),
+            new SolidColorBrush(Color.FromArgb(255, 109, 135, 100)),
+            new SolidColorBrush(Color.FromArgb(255, 100, 118, 135)),
+            new SolidColorBrush(Color.FromArgb(255, 118, 96, 138)),
+            new SolidColorBrush(Color.FromArgb(255, 135, 121, 78))
         };
 
         private ColorsHelper() { }
