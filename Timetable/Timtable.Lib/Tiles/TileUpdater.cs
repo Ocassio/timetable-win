5efb110a79c19e17a4aebb9dfc0e138208a5c114
Alexander Ionov <orlicus@gmail.com>
2015-08-04 10:10:38 +0400

    More than 4 lessons tile handling (fix is needed)

---
+++ Timetable/Timtable.Lib/Tiles/TileUpdater.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using Timetable.Providers;
+using Timetable.Utils;
+
+namespace Timtable.Lib.Tiles
+{
+    public class TileUpdater
+    {
+        public static async Task UpdateTile()
+        {
+            var updater = TileUpdateManager.CreateTileUpdaterForApplication();
+            updater.EnableNotificationQueue(true);
+            updater.Clear();
+
+            var imageTileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Image);
+            var image = (XmlElement)imageTileXml.GetElementsByTagName("image")[0];
+            image.SetAttribute("src", "ms-appx:///Assets/Wide310x150Logo.scale-100.png");
+            updater.Update(new TileNotification(imageTileXml));
+
+            var group = SettingsProvider.Group;
+            if (group == null) return;
+
+            var dateRange = DateUtils.GetCustomDateRange();
+
+            try
+            {
+                var days = await DataProvider.GetTimetableByGroup(group.Id, dateRange);
+                if (days.Count > 0)
+                {
+                    var lessons = days[0].Lessons;
+                    var tilesCount = 0;
+                    var lineNumber = 1;
+                    var lessonNumber = 0;
+                    while (lessonNumber < lessons.Count)
+                    {
+                        var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Text01);
+                        var textLines = tileXml.GetElementsByTagName("text");
+                        textLines[0].InnerText = "Сегодня";
+
+                        if (lineNumber == 0)
+                        {
+                            lineNumber++;
+                            textLines[lineNumber].InnerText = "...";
+                        }
+
+                        var lessonsRemains = lessons.Count - lessonNumber;
+                        var linesRemains = 4 - lineNumber;
+                        while (lineNumber < Math.Min(linesRemains, lessonsRemains) + 1)
+                        {
+                            textLines[lineNumber].InnerText = lessons[lessonNumber].Number + ") " + lessons[lessonNumber].Name;
+                            lineNumber++;
+                            lessonNumber++;
+                        }
+
+                        if (lineNumber == 4)
+                        {
+                            if (lessonNumber == lessons.Count - 1)
+                            {
+                                textLines[lineNumber].InnerText = lessons[lessonNumber].Number + ") " + lessons[lessonNumber].Name;
+                                lessonNumber++;
+                            }
+                            else
+                            {
+                                textLines[lineNumber].InnerText = "...";
+                                lineNumber = 0;
+                            }
+                        }
+
+                        tilesCount++;
+                        if (tilesCount <= 4)
+                        {
+                            updater.Update(new TileNotification(tileXml));
+                        }
+                    }
+                }
+                else
+                {
+                    var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Text01);
+                    var textLines = tileXml.GetElementsByTagName("text");
+                    textLines[0].InnerText = "Сегодня";
+                    textLines[1].InnerText = "Пары отсутствуют";
+                    updater.Update(new TileNotification(tileXml));
+                }
+                
+            }
+            catch (WebException) { }
+        }
+    }
+}
